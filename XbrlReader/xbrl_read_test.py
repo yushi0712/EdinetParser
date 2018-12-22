be0f06584fa66bf5a1481ca0adddea374f22fa12
nishimura <yushi.nishimura@konicaminolta.com>
2018-12-22 22:14:30 +0900

    \を/に変更

--- XbrlReader/xbrl_read_test.py
+++ XbrlReader/xbrl_read_test.py
@@ -1,5 +1,5 @@
 # xbrl から読み込む
 from xbrl_proc import read_xbrl
 
-xbrl_file = r"C:\local\users\nishimura\Programs\!Python\sandbox\Edinet\XBRL\jpcrp030000-asr-001_E00989-000_2018-03-31_01_2018-06-20.xbrl"
+xbrl_file = r"C:/local/users/nishimura/Documents/!Theme/財務分析/XBRL/E00989_コニカミノルタ株式会社/jpcrp030000-asr-001_E00989-000_2018-03-31_01_2018-06-20.xbrl"
 df = read_xbrl(xbrl_file)
